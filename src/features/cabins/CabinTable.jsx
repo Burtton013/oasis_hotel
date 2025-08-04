5c5fec3944c547c425a309e5a045963ec49f7ec1
Oliver B <cbop95@gmail.com>
2025-08-04 12:26:01 -0600

    Implementacion de React Query para el manejo de state remoto

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import styled from "styled-components";
+import { getCabins } from "../../services/apiCabins";
+
+import Spinner from "../../ui/Spinner";
+import CabinRow from "./CabinRow";
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -23,3 +28,39 @@ const TableHeader = styled.header`
   color: var(--color-grey-600);
   padding: 1.6rem 2.4rem;
 `;
+
+function CabinTable() {
+  //Hook que nos permite traer, almacenar en cache y sincronizar los datos remotos
+  const {
+    //Desestructuramos de una vez las propiedades que necesitamos de la data retornada en queryFn
+    isLoading,
+    data: cabins,
+    // error,
+  } = useQuery({
+    //Key para identificar de manera unica o si cambio el state relacionado a esta key
+    queryKey: ["cabin"],
+    //propiedad asincrona donde se hace el fetch o bien se almacena la funcion que hara fetch
+    //retorna una promesa, dentro de la data que retorna ya lleva los loading entre otros estados
+    queryFn: getCabins,
+  });
+
+  if (isLoading) return <Spinner />;
+  console.log(cabins);
+  return (
+    <Table role="table">
+      <TableHeader role="row">
+        <div></div>
+        <div>Cabin</div>
+        <div>Capacity</div>
+        <div>Price</div>
+        <div>Discount</div>
+        <div></div>
+      </TableHeader>
+      {cabins.map((cabin) => (
+        <CabinRow cabin={cabin} key={cabin.id} />
+      ))}
+    </Table>
+  );
+}
+
+export default CabinTable;
